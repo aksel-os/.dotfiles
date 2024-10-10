e7ee988515036bc143e1701ff2fc9827fae1cc57
Aksel Steen - Mac <akselolav@gmail.com>
2024-10-10 22:48:41 +0200

    initial commit

--- flake.nix
+++ flake.nix
@@ -1,51 +1,52 @@
 {
-  description = "Hubble Darwin system flake";
+
+  description = "Hubble Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixpkgs/unstable";
+    
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
+  outputs = inputs@{ self, nix-darwin, nixpkgs, ... } {
+    let
+      # --- SYSTEM SETTINGS --- #
+      systemSettings = {
+        system = "aarch64-darwin"
+        hostname = "Hubble"
+        timezone = ""
+        locale = ""
+      };
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
+      # ---- USER SETTINGS ---- #
+      userSettings = {
+        dotfilesDir = "~/.dotfiles"
+        theme = "catppuccin"
+        term = "iTerm2"
+        font = ""
+      };
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+      # --- ---#
+      lib = nixpkgs.lib;
+      
+    in
+    darwinConfigurations = let
+      inherit (inputs.nix-darwin.lib) darwinSystem
+      in {
+        Hubble = darwinSystem {
+          system = system;
+	  specialArgs = { inherit inputs; };
+          modules = [
+	    ./configuration.nix
+	    
+	  ];
+	};
+      };
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#Hubble
-    darwinConfigurations."Hubble" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-
-    # Expose the package set, including overlays, for convenience.
     darwinPackages = self.darwinConfigurations."Hubble".pkgs;
   };
 }
