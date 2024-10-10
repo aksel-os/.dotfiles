3201a733b696bbf6b56fdfae31629d4940ee1379
Aksel Steen - Mac <akselolav@gmail.com>
2024-10-10 23:49:24 +0200

    added zsh

--- home.nix
+++ home.nix
@@ -71,6 +71,17 @@
     # EDITOR = "emacs";
   };
 
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      javacc = "javac *.java";
+      javacleaner = "rm *.class";
+      IN1000 = "cd ~/UiO/Semester_1/IN1000/";
+      IN1020 = "cd ~/UiO/Semester_1/IN1020/";
+      IN1140 = "cd ~/UiO/Semester_1/IN1140/";
+    };
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
