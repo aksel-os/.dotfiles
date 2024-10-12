2e10e1c861bc639255e5b33fc08f12708806e52b
Aksel Steen - Mac <akselolav@gmail.com>
2024-10-12 22:34:57 +0200

    re-ordered the config for kepler

--- hosts/kepler/configuration.nix
+++ hosts/kepler/configuration.nix
@@ -1,6 +1,17 @@
-{ config, pkgs, ...}:
+{ config, pkgs, lib, userSettings, systemSettings, ...}:
 {
+
+  imports = [
+
+  ];
+
   services.nix-daemon.enable = true;
+
+  # Flakes
+  nix.extraOptions = ""
+    experimental-features = nix-command flakes
+  "";
+
   system.stateVersion = 5;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  
 }
