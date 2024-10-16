3a11aed0f2a4b51657b42966cfa42a38b38c07f1
Aksel Steen - Mac <akselolav@gmail.com>
2024-10-16 20:19:27 +0200

    testing emacs

--- packages/emacs/emacs.nix
+++ packages/emacs/emacs.nix
@@ -6,7 +6,14 @@
 
   programs.emacs = {
     enable = true;
-    
+    package = pkgs.emacs-pgtk;
+    extraConfig = builtins.readFile ./init.el; 
+    extraPackages = (epkgs: (with epkgs; [
+      vertico savehist consult which-key
+      magit
+      nix-mode org-mode
+      catppuccin-theme
+    ]))
   };
 
 }
---
+++ packages/emacs/init.el
@@ -0,0 +1,93 @@
+;; Dette fjerner velkomstskjermen ved oppstart av Emacs.
+(setq inhibit-splash-screen t)
+
+;; Emacs ber deg iblant svare "yes" eller "no".  Dette gjør at Emacs
+;; nøyer seg med "y" eller "n".
+(setq use-short-answers t)
+
+;; Skru av blinkende peker.
+(blink-cursor-mode 0)
+
+;; Disse skrur av diverse GUI-elementer.
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+
+(global-auto-revert-mode)
+(setq global-auto-revert-non-file-buffers t
+      create-lockfiles nil)
+(setq custom-file (locate-user-emacs-file "custom.el")) 
+(load custom-file t t)
+
+
+(setq backup-directory-alist `(("." . ,(locate-user-emacs-file "backups"))))
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-refresh-contents)
+(package-initialize)
+;; (use-package my-package-name
+;;   :ensure t    ; Ensure my-package is installed
+;;   :after foo   ; Load my-package after foo is loaded (seldom used)
+;;   :init        ; Run this code before my-package is loaded
+;;   :bind        ; Bind these keys to these functions
+;;   :custom      ; Set these variables
+;;   :config      ; Run this code after my-package is loaded
+
+(unless (package-installed-p 'use-package))
+(package-install 'use-package)
+
+(require 'use-package)
+
+;; Python LSP
+(use-package lsp-pyright
+  :ensure t
+  :custom (lsp-pyright-langserver-command "pyright") ;; or basedpyright
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-pyright)
+                          (lsp))))  ; or lsp-deferred
+
+;; VERTical Interactive COmpletion
+(use-package vertico
+  :ensure t
+  :config
+  (setq vertico-count 25)
+  (vertico-mode 1))
+
+;; Save minibuffer history
+(use-package savehist
+  :config
+  (savehist-mode 1))
+
+;; Forbedrer mange kommandoer når du skal navigere og søke
+(use-package consult
+  :ensure t
+  :bind ("C-x b" . consult-buffer)
+  :config
+  (setq consult-preview-key (list :debounce 0.1 'any)))
+
+;; Tidligere besøkte filer
+(recentf-mode t)
+(setq recentf-max-saved-items 1000)
+
+;; Display available keybindings in popup
+(use-package which-key
+  :ensure t
+  :config (which-key-mode 1))
+
+;; Themes
+(use-package catppuccin-theme
+  :ensure t
+  :config
+  (load-theme 'catppuccin :no-confirm)
+  (setq catppuccin-flavor 'mocha)) ;; or 'latte, 'macchiato, or 'mocha)
+
+;; Multiple Cursors
+(use-package multiple-cursors
+  :ensure t)
+
+;; Linjenummer - Relative
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+(setq display-line-numbers-type 'relative)
+
+(show-paren-mode 1) 
