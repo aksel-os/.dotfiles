35681fbb05fecb756b1b738645c8873151e7b05e
Aksel Steen - Mac <akselolav@gmail.com>
2025-02-17 01:58:11 +0100

    removed old .el files as my emacs now has its own repo. Added .gitmodules with emacs repo as submodule, finally added readme

--- modules/emacs/emacs.nix
+++ modules/emacs/emacs.nix
@@ -8,7 +8,7 @@
   programs.emacs =  { 
     enable = true;
     package = (pkgs.emacsWithPackagesFromUsePackage {
-      config = "github:aksel-os/.emacs.d/init.org";     
+      config = "./init.org";     
       package = pkgs.emacs-git;
       defaultInitFile = true;
       alwaysEnsure = true;
