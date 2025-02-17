852a07e4d594ec151c20fae9c60fe3a79d1d3039
Aksel Steen - Mac <akselolav@gmail.com>
2025-02-17 01:47:40 +0100

    sync

--- modules/emacs/emacs.nix
+++ modules/emacs/emacs.nix
@@ -8,12 +8,14 @@
   programs.emacs =  { 
     enable = true;
     package = (pkgs.emacsWithPackagesFromUsePackage {
-      # config = "github:aksel-os/.emacs.d/init.org";
-      config = "init.org";
+      config = "github:aksel-os/.emacs.d/init.org";     
       package = pkgs.emacs-git;
       defaultInitFile = true;
       alwaysEnsure = true;
       alwaysTangle = true;
+      extraEmacsPackages = epkgs: [            
+        epkgs.jinx
+      ];
     });
   };
 }
