07ad9954d77ec5f050d46aea5cf4c5496f3466c2
Aksel Steen - Mac <akselolav@gmail.com>
2025-02-21 22:39:55 +0100

    removed colorsprites

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -35,7 +35,7 @@ in {
     enable = true;
     shellAliases = myAliases;
     initExtra = ''
-      eval "$(${pkgs.oh-my-posh}/bin/oh-my-posh init zsh --config ${zen-omp})"
+      eval "$(${pkgs.oh-my-posh}/bin/oh-my-posh init zsh --config ${zen-omp})"      
     '';
   };
   programs.bash = {
