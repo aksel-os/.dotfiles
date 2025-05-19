e7190e64de30342db05826eaaf07e803c08f70a6
Aksel O. Steen <aksel@akselos.no>
2025-05-19 22:06:19 +0200

    devshell: use zsh

--- modules/flake-parts/shell.nix
+++ modules/flake-parts/shell.nix
@@ -5,9 +5,13 @@
         default = pkgs.mkShellNoCC {
           name = "dotfiles-shell";
           meta.description = "Dev environment for dotfiles";
+          shellHook = ''
+          $/bin/zsh
+          '';
           packages = with pkgs; [
             just
             git
+            zsh
           ];
         };
       };
