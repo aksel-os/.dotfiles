66a5ad0d023cf8ff6c4197539dd371aea300e9ca
Aksel O. Steen <aksel@akselos.no>
2025-06-03 10:13:54 +0200

    formatting: used alejandra

--- modules/home/core/editors/emacs.nix
+++ modules/home/core/editors/emacs.nix
@@ -1,29 +1,33 @@
-{ pkgs, inputs, ...}:
-
-let
-  emacs-pkg = if pkgs.stdenv.isLinux
-              then pkgs.emacs-pgtk
-              else
-                (pkgs.emacs-git).overrideAttrs (o: {
-                  patches = [
-                    "${inputs.emacs-plus}/patches/emacs-31/fix-window-role.patch"
-                    "${inputs.emacs-plus}/patches/emacs-31/round-undecorated-frame.patch"
-                    "${inputs.emacs-plus}/patches/emacs-31/system-appearance.patch"
-                  ];
-                });
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  emacs-pkg =
+    if pkgs.stdenv.isLinux
+    then pkgs.emacs-pgtk
+    else
+      (pkgs.emacs-git).overrideAttrs (o: {
+        patches = [
+          "${inputs.emacs-plus}/patches/emacs-31/fix-window-role.patch"
+          "${inputs.emacs-plus}/patches/emacs-31/round-undecorated-frame.patch"
+          "${inputs.emacs-plus}/patches/emacs-31/system-appearance.patch"
+        ];
+      });
 in {
-  programs.emacs =  { 
+  programs.emacs = {
     enable = true;
-    package = (pkgs.emacsWithPackagesFromUsePackage {
+    package = pkgs.emacsWithPackagesFromUsePackage {
       config = "${inputs.emacs-config}/init.org";
       package = emacs-pkg;
       defaultInitFile = true;
       alwaysEnsure = true;
       alwaysTangle = true;
-      extraEmacsPackages = epkgs: with epkgs; [
-        jinx
-        vterm
-      ];
-    });
+      extraEmacsPackages = epkgs:
+        with epkgs; [
+          jinx
+          vterm
+        ];
+    };
   };
 }
