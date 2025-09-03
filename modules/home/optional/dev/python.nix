625cb9b947819db19ca826f92b276249b4d3f784
Aksel O. Steen <aksel@akselos.no>
2025-09-03 14:42:47 +0200

    feat(python): added pylint

--- modules/home/optional/dev/python.nix
+++ modules/home/optional/dev/python.nix
@@ -1,11 +1,9 @@
-{ pkgs, ...}:
-
-{
+{pkgs, ...}: {
   home.packages = with pkgs; [
     python312
     pyright
+    pylint
     python312Packages.autopep8
-    python312Packages.nltk
     python312Packages.numpy
     python312Packages.pip
   ];
