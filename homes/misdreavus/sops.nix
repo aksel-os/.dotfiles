d34ee815596e1e17ff55e8617041003b66f21353
Aksel O. Steen <aksel@akselos.no>
2025-09-18 12:06:11 +0200

    feat: working on nixos

--- homes/misdreavus/sops.nix
+++ homes/misdreavus/sops.nix
@@ -1,10 +1,16 @@
-{ inputs, ... }:
+{ inputs, config, ... }:
 
 {
   sops = {
-    age.keyFile = "/var/lib/sops-nix/key.txt";
+    # age.keyFile = "/var/lib/sops-nix/key.txt";
+    age.sshKeyPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
 
     defaultSopsFile = "${inputs.secrets}/secrets.yaml";
-    validateSopsFiles = false;
+
+    secrets = {
+      "keys/ssh/github" = { };
+
+      "keys/ssh/uio" = { };
+    };
   };
 }
