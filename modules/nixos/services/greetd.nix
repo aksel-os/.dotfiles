c24f493bcd1e97d439533d57d797f9dd8518ac09
Aksel O. Steen <aksel@akselos.no>
2025-09-18 15:42:14 +0200

    feat: made tuigreet the default greeter

--- modules/nixos/services/greetd.nix
+++ modules/nixos/services/greetd.nix
@@ -4,7 +4,8 @@
   services.greetd = {
     enable = true;
     settings.default_session = {
-      command = "hyprland";
+      # command = "hyprland";
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session --sessions ${pkgs.hyprland}/share/wayland-sessions";
       user = "misdreavus";
     };
   };
