09205fe1a4b6e778eb896ca4619e31dac8fadec2
Aksel O. Steen <aksel@akselos.no>
2025-11-10 13:50:37 +0100

    feat(aliases): update shell aliases

--- modules/home/core/shell/aliases.nix
+++ modules/home/core/shell/aliases.nix
@@ -4,19 +4,26 @@
     evim = "emacs -nw";
 
     cd = "z";
+    ".." = "cd ..";
+    "..." = "cd ../..";
+    "...." = "cd ../../..";
+    "-" = "cd -";
+
     bat = "bat --paging=never --style=plain";
     cat = "bat";
+    icat = "kitten icat"; # Show images in Kitty
+
     ls = "eza -lT --icons -L=1 --hyperlink";
     la = "ls -a";
     ll = "eza -lT --icons -L=1 --git-repos --git  --hyperlink";
     lla = "ll -a";
     tree = "eza --tree --icons";
-    htop = "btop";
+
     gitfetch = "onefetch";
 
     javacc = "javac *.java";
     javacleaner = "rm *.class";
 
-    hawk = "git";
+    py = "python3";
   };
 }
