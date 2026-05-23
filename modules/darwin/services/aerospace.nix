a259d865dea8cd3416e0fe1237df4a2ac971cf73
Aksel O. Steen <aksel@akselos.no>
2026-05-23 17:03:00 +0200

    feat: add ability to move workspaces to aerospace

--- modules/darwin/services/aerospace.nix
+++ modules/darwin/services/aerospace.nix
@@ -23,15 +23,24 @@
         alt-cmd-k = "resize height -60";
         alt-cmd-l = "resize width +60";
 
+        alt-ctrl-cmd-h = "move-workspace-to-monitor left";
+        alt-ctrl-cmd-j = "move-workspace-to-monitor down";
+        alt-ctrl-cmd-k = "move-workspace-to-monitor up";
+        alt-ctrl-cmd-l = "move-workspace-to-monitor right";
+
+        alt-cmd-1 = "summon-workspace 1";
+        alt-cmd-2 = "summon-workspace 2";
+        alt-cmd-3 = "summon-workspace 3";
+        alt-cmd-4 = "summon-workspace 4";
+        alt-cmd-5 = "summon-workspace 5";
+        alt-cmd-6 = "summon-workspace 6";
+
         alt-1 = "workspace 1";
         alt-2 = "workspace 2";
         alt-3 = "workspace 3";
         alt-4 = "workspace 4";
         alt-5 = "workspace 5";
         alt-6 = "workspace 6";
-        alt-7 = "workspace 7";
-        alt-8 = "workspace 8";
-        alt-9 = "workspace 9";
 
         alt-shift-1 = "move-node-to-workspace 1";
         alt-shift-2 = "move-node-to-workspace 2";
@@ -39,9 +48,6 @@
         alt-shift-4 = "move-node-to-workspace 4";
         alt-shift-5 = "move-node-to-workspace 5";
         alt-shift-6 = "move-node-to-workspace 6";
-        alt-shift-7 = "move-node-to-workspace 7";
-        alt-shift-8 = "move-node-to-workspace 8";
-        alt-shift-9 = "move-node-to-workspace 9";
       };
     };
   };
