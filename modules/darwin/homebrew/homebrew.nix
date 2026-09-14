3d0e9400802d5a8994d8db223f848847a1f87a0d
Aksel O. Steen <aksel@akselos.no>
2026-09-14 09:59:46 +0200

    chore: remove unused jetbrains tap

--- modules/darwin/homebrew/homebrew.nix
+++ modules/darwin/homebrew/homebrew.nix
@@ -43,13 +43,6 @@
           hash = "sha256-XGvgNHmh5/2Z5vonSw3DvsIlEoFhQWua4M/1DYWGcco=";
         };
 
-        # "Jetbrains/utils" = pkgs.fetchFromGitHub {
-        #   owner = "JetBrains";
-        #   repo = "homebrew-utils";
-        #   rev = "e664ad2d3d434fa4a24fdf0f9d8f649d5a801161"; # main 20.05.2026
-        #   hash = "sha256-jVFkLMIazYsAsdfvCYCg7u6kSfXan1Gv6f1KP4u3eAE=";
-        # };
-
         "homebrew-zathura/homebrew-zathura" = pkgs.fetchFromGitHub {
           owner = "homebrew-zathura";
           repo = "homebrew-zathura";
