e97512e5fa73ba830caef8befc28b4d57456e102
Aksel O. Steen <aksel@akselos.no>
2026-09-21 09:30:01 +0200

    feat: add manpage ordering

--- modules/core/environment/environment.nix
+++ modules/core/environment/environment.nix
@@ -14,6 +14,7 @@ in
     VISUAL = "nvim";
     SUDO_EDITOR = "nvim";
 
+    MANSECT = "2:3:1:8:5:4:7:6:9:3P";
     MANPAGER = mkIf pkgs.stdenv.isLinux "sh -c 'col -bx | bat -l man -p'";
     MANROFFOPT = mkIf pkgs.stdenv.isLinux "-c";
   };
